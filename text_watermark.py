6d4aede4aa17057894f1c50a9a5ba513ce74f394
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-08-05 15:29:35 +0530

    Implemented watermark using text

---
+++ text_watermark.py
@@ -0,0 +1,43 @@
+# import all the libraries
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
+import matplotlib.pyplot as plt
+import numpy as np
+
+# image opening
+image = Image.open("wallpaper.jpg")
+# this open the photo viewer
+# image.show()  
+
+# text Watermark
+watermark_image = image.copy()
+
+draw = ImageDraw.Draw(watermark_image)
+# ("font type",font size)
+w, h = image.size
+x, y = int(w / 2), int(h / 2)
+if x > y:
+  font_size = y
+elif y > x:
+  font_size = x
+else:
+  font_size = x
+  
+font = ImageFont.truetype("arial.ttf", int(font_size/6))
+
+# add Watermark
+# (0,0,0)-black color text
+draw.text((x, y), "titan", fill=(0, 0, 0), font=font, anchor='ms')
+plt.subplot(1, 2, 1)
+plt.title("black text")
+plt.imshow(watermark_image)
+
+# add Watermark
+# (255,255,255)-White color text
+draw.text((x, y), "titan", fill=(255, 255, 255), font=font, anchor='ms')
+plt.subplot(1, 2, 2)
+plt.title("white text")
+plt.imshow(watermark_image)
+
+plt.show()
