8169fd3036ee2cc8712de7eeeb477c6f8876d44a
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-08-05 15:30:07 +0530

    implemented watermark using image

---
+++ image_watermark.py
@@ -0,0 +1,25 @@
+# import all the libraries
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+
+# to open the image
+image = Image.open("wallpaper.jpg")
+# this open the photo viewer
+# image.show()
+plt.imshow(image)
+
+# image watermark
+size = (500, 100)
+crop_image = image.copy()
+# to keep the aspect ration in intact
+crop_image.thumbnail(size)
+
+# add watermark
+copied_image = image.copy()
+# base image
+copied_image.paste(crop_image, (500, 200))
+# pasted the crop image onto the base image
+plt.imshow(copied_image)
+
+plt.show()
